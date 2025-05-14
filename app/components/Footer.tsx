db5d6f75000c853e0bd8d1569a7793b36fe9cd12
reese-code <reese.coding@gmail.com>
2025-05-14 13:21:04 -0600

    footer adjusted fontsize

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -28,9 +28,9 @@ export default function Footer({ socialLinks: propSocialLinks }: FooterProps) {
 
   return (
     <footer className="py-16 px-4 sm:px-10 ">
-      <div className="flex w-full gap-10 mb-16">
+      <div className="flex w-full gap-5 md:gap-10 mb-16">
         {/* Menu Links */}
-        <div className="w-1/3">
+        <div className="w-1/2 md:w-1/3">
           <h3 className="text-xl mb-4 border-b border-black pb-2">Menu</h3>
           <ul className="flex flex-col gap-[10px]">
             <li>
@@ -61,7 +61,7 @@ export default function Footer({ socialLinks: propSocialLinks }: FooterProps) {
         </div>
 
         {/* Social Links */}
-        <div className="w-1/3">
+        <div className="w-1/2 md:w-1/3">
           <h3 className="text-xl mb-4 border-b border-black pb-2 ">Socials</h3>
           <ul className="flex flex-col gap-[10px]">
             {socialLinks && socialLinks.map((link, index) => (
@@ -83,46 +83,46 @@ export default function Footer({ socialLinks: propSocialLinks }: FooterProps) {
       {/* Bottom Section */}
       <div className="flex w-auto flex-col">
         {/* First Line */}
-        <div className="flex items-center flex-wrap gap-6">
-          <img src="/images/mountain.svg" alt="Mountain" className="w-[78px] h-auto" />
-          <span className="text-[65px] font-light leading-none font-editorial">Denver</span>
+        <div className="flex items-center flex-wrap gap-4">
+          <img src="/images/mountain.svg" alt="Mountain" className="w-[48px] md:w-[78px] h-auto" />
+          <span className="text-[36px] md:text-[65px] font-light leading-none font-editorial">Denver</span>
 
-          <div className="text-[23px] font-neue w-[220px]">
+          <div className="text-[23px] font-neue w-[140px] md:w-[220px]">
             <div className="flex flex-row justify-between">
-              <img src="/images/arrow.svg" alt="Arrow Left" className="w-6 h-auto rotate-180" />
-              <div className='text-[23px] font-neue'>39.7392</div>
-              <img src="/images/arrow.svg" alt="Arrow Left" className="w-6 h-auto " />
+              <img src="/images/arrow.svg" alt="Arrow Left" className="w-4 md:w-6 h-auto rotate-180" />
+              <div className='text-[16px] md:text-[23px] font-neue'>39.7392</div>
+              <img src="/images/arrow.svg" alt="Arrow Left" className="w-4 md:w-6 h-auto " />
             </div>
             <div className="flex flex-row justify-between">
-              <img src="/images/arrow.svg" alt="Arrow Right" className="w-6 h-auto" />
-              <div className='text-[23px] font-neue'>104.9903</div>
-              <img src="/images/arrow.svg" alt="Arrow Right" className="w-6 h-auto rotate-180" />
+              <img src="/images/arrow.svg" alt="Arrow Right" className="w-4 md:w-6 h-auto" />
+              <div className='text-[16px] md:text-[23px] font-neue'>104.9903</div>
+              <img src="/images/arrow.svg" alt="Arrow Right" className="w-4 md:w-6 h-auto rotate-180" />
             </div>
           </div>
 
-          <span className=" font-light leading-none text-[65px] font-editorial">Colorado</span>
-          <img src="/images/state_colorado.svg" alt="Colorado" className="w-[63px] h-auto" />
-          <img src="/images/C.svg" alt="C" className="w-[77px] h-auto" />
-          <img src="/images/O.svg" alt="O" className="w-[77px] h-auto" />
+          <span className=" font-light leading-none text-[36px] md:text-[65px] font-editorial">Colorado</span>
+          <img src="/images/state_colorado.svg" alt="Colorado" className="w-[42px] md:w-[63px] h-auto" />
+          <img src="/images/C.svg" alt="C" className="w-[42px] md:w-[77px] h-auto" />
+          <img src="/images/O.svg" alt="O" className="w-[42px] md:w-[77px] h-auto" />
         </div>
 
         {/* Second Line */}
-        <div className="flex items-center flex-wrap gap-6 border-b pb-6">
-          <Link to="/about" className="group font-light inline-block text-[65px] font-editorial transition-all duration-300 hover:underline">
-            Creative Studio <span className="font-light font-neue text-[23px]">BY</span> Reese Latimer
+        <div className="flex items-center flex-wrap gap-4 border-b pb-6">
+          <Link to="/about" className="group font-light inline-block text-[36px] md:text-[65px] font-editorial transition-all duration-300 hover:underline">
+            Creative Studio <span className="font-light font-neue text-[16px] md:text-[23px]">BY</span> Reese Latimer
           </Link>
 
           {/* Star SVG */}
-          <img src="/images/Star.svg" alt="Star" className="w-[52px] h-auto font-[65px] " />
+          <img src="/images/Star.svg" alt="Star" className="w-[32px] md:w-[52px] h-auto text-[36px] md:text-[65px] " />
 
           {/* Work Together */}
-          <Link to="/contact" className="flex items-start gap-2 text-[65px] font-editorial font-light hover:underline transition-all duration-300">
+          <Link to="/contact" className="flex items-start gap-2 text-[36px] md:text-[65px] font-editorial font-light hover:underline transition-all duration-300">
             <span>work</span>
-            <img src="/images/connecting_arrow.svg" alt="Connecting Arrow" className="w-[66px] h-auto" />
+            <img src="/images/connecting_arrow.svg" alt="Connecting Arrow" className="w-[40px] md:w-[66px] h-auto" />
             <span>together</span>
           </Link>
 
-          <div className="ml-auto">
+          <div className="ml-auto text-type-small">
             <span>©2024</span>
           </div>
         </div>
